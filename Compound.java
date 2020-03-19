0d73dec8bd8d1da5388aeb5a39ee5754c0e69992
tlsimala <50175853+tlsimala@users.noreply.github.com>
2020-03-19 00:11:23 -0400

    Update Compound.java
    finished

--- Compound.java
+++ Compound.java
@@ -1,7 +1,7 @@
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
-
 /**
  * @author tlsimala
  * 
@@ -11,6 +11,7 @@ import java.util.Set;
 public class Compound {
 	private String ID;
 	private HashMap<Plate, ArrayList<Well>> storage;
+
 	/**
 	 * The constructor
 	 * @param idString
@@ -19,14 +20,14 @@ public class Compound {
 		this.ID=idString;
 		this.storage=new HashMap<Plate, ArrayList<Well>>();
 	}
-
+	
 	/**
 	 * This method adds a plate into the hashmap
 	 * @param plate
 	 */
 	public void AddPlate(Plate plate) {
-	  ArrayList<Well> wellList=new ArrayList<Well>();
-	  storage.put(plate, wellList);
+		ArrayList<Well> wellList=new ArrayList<Well>();
+		storage.put(plate, wellList); //puts the plate in the hashmap and creates an arraylist for the particular plate
 	}
 	
 	/**
@@ -35,8 +36,8 @@ public class Compound {
 	 * @param well
 	 */
 	public void AddWell(Plate plate, Well well) {
-	   ArrayList<Well> cloneWellList=storage.get(plate);
-	   cloneWellList.add(well);
+		ArrayList<Well> cloneWellList=storage.get(plate);
+		cloneWellList.add(well); //adds well in the arraylist 
 	}
 	
 	/**
@@ -45,15 +46,15 @@ public class Compound {
 	 * @return true or false
 	 */
 	public boolean findWell(String wellID) {
-	  Set<Plate> plateSet=storage.keySet(); //a set of plates
-	   for(Plate plate: plateSet) {
-		ArrayList<Well> wellList=storage.get(plate);
-		for(Well well: wellList) { //going through the well list for the plates
-		   if(well.getPlateID().equals(wellID)) {
-			return true;
-	           }
-	         }
-	      }
+		Set<Plate> plateSet=storage.keySet(); //a set of plates
+		for(Plate plate: plateSet) {
+			ArrayList<Well> wellList=storage.get(plate);
+			for(Well well: wellList) { //going through the well list for the plates
+				if(well.getPlateID().equals(wellID)) {
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 	
@@ -63,13 +64,13 @@ public class Compound {
 	 * @return Plate
 	 */
 	public Plate findPlate(String plateID) {
-	  Set<Plate> plateSet=storage.keySet();
-	  Plate clonePlate=null;
-	   for(Plate plate: plateSet) {
-	      if(plate.getFullID().equals(plateID)) {
-		clonePlate=plate;
+		Set<Plate> plateSet=storage.keySet();
+		Plate clonePlate=null;
+		for(Plate plate: plateSet) {
+			if(plate.getFullID().equals(plateID)) {
+				clonePlate=plate;
+			}
 		}
-	     }
 		return clonePlate;   
 	}
 	
@@ -78,6 +79,6 @@ public class Compound {
 	 * @return the compound ID
 	 */
 	public String toString() {
-	   return ID;
+		return ID;
 	}
 }
