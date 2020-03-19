5c662eb924a28ecda3d6e127250c6289506dc86f
tlsimala <50175853+tlsimala@users.noreply.github.com>
2020-03-18 23:40:33 -0400

    Update KaliedoCodingClient.java
    skeleton

--- KaliedoCodingClient.java
+++ KaliedoCodingClient.java
@@ -57,3 +57,83 @@ public class KaleidoClientCode {
 			break;
 		}
 	}
+	
+		
+	/**
+	 * This method registers a CompoundID
+	 * @param scanner
+	 */
+	public static void registration(Scanner scanner) {
+
+	}
+	
+	/**
+	 * This method assigns a CompoundID to a Plate
+	 * @param scanner
+	 */
+	public static void assignment(Scanner scanner) {
+
+	}
+	
+	/**
+	 * This is a helper method for the assignment method that creates a new plate and well object
+	 * and then adds it into the specific compound object. 
+	 * @param ID
+	 * @param plateID
+	 */
+	public static void assignmentHelper(String ID, String plateID) {
+
+	}
+	
+
+	/**
+	 * This method is to transfer content to and from a number of Wells. 
+	 * @param scanner
+	 */
+	public static void transfer(Scanner scanner) {
+
+	}
+	
+	/**
+	 * This is the helper method which gets the compound the wells are located in and
+	 * transfers the content.
+	 * @param ID
+	 * @param wells
+	 */
+	public static void transferHelper(String ID, String wells) {
+
+	}
+	
+	/**
+	 * This is a helper method that returns the correct compound.
+	 * @param plateID
+	 * @return
+	 */
+	public static Compound getCompound(String plateID) {
+
+	}
+	
+	/**
+	 * This is a method that gets the compoundId from a particular Well.
+	 * @param scanner
+	 */
+	public static void request(Scanner scanner) {
+
+	}
+	
+	/**
+	 * This is a helper method that returns the compound the well is in.
+	 * @param ID
+	 * @return compound
+	 */
+	public static Compound requestHelper(String ID) {
+
+	}
+	
+	/**
+	 * This is a method that prints out the end prompt. 
+	 */
+	public static void finish() {
+		
+	}
+}
